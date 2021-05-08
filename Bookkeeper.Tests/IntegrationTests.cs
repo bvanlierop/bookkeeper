81bdb757c75fe779ea741de9bb9d49786970e2cf
Bart van Lierop <bartvanlierop@gmail.com>
2021-05-08 14:35:48 +0200

    Loads json category map in seperate test file.

--- Bookkeeper.Tests/IntegrationTests.cs
+++ Bookkeeper.Tests/IntegrationTests.cs
@@ -11,34 +11,15 @@ namespace Bookkeeper.Tests
         [Test]
         public void ShouldProcessOfficialAbnAmroBankExportFile()
         {
-            // TODO: no real integration test if just reading from disk (should use file abstraction here)
             var inputFileData = File.ReadAllText($@"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\TestFiles\THREE_MONTH_ABN_AMRO_EXPORT_FILE.TAB");
-
-            var categoryMapJsonString = 
-                "{" +
-                    "\"Categories\": " +
-                    "[ " +
-                        "{" +
-                            "\"Keyword\": \"Cars are Us\"," +
-                            "\"CategoryName\": \"car\" " +
-                        "}," +
-                        "{" +
-                            "\"Keyword\": \"Zorgverzekeringen\"," +
-                            "\"CategoryName\": \"healthcare\" " +
-                        "}," +
-                        "{" +
-                            "\"Keyword\": \"Contactalook\"," +
-                            "\"CategoryName\": \"healthcare\" " +
-                        "}" +
-                    "]" +
-                "}";
+            var inputCategoryMapJson = File.ReadAllText($@"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\TestFiles\TestCategories.json");
 
             var parser = new TransactionParser(inputFileData);
-            var processor = new TransactionProcessor(parser, categoryMapJsonString);
+            var processor = new TransactionProcessor(parser, inputCategoryMapJson);
             var cr = new CategoryReporter(processor);
+
             var report = cr.CreateReport();
 
-            // TODO: split up expenses with income
             StringAssert.Contains("unknown: 2453.43 EUR", report);
         }
     }
